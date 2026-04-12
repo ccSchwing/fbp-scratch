9e274d3677b261c1077341c338414e0ba3a63386
Chuck Schwing <chuckschwing@proton.me>
2026-04-12 08:49:28 -0400

    Added the addFBPUser method.  Mdified both yamls to accoumodate the new method and endpoint.

--- src/AddOrUpdateFBPUser/src/AddOrUpdateFBPUser.py
+++ src/AddOrUpdateFBPUser/src/AddOrUpdateFBPUser.py
@@ -105,6 +105,82 @@ def updateFBPUser():
                 })
             } 
 
+
+
+@app.post("/addFBPUser")
+def addFBPUser():
+    logger.info("Handling addFFBPUser request")  # Log entry into the function
+    try:
+        logger.info(f"Raw event data: {json.dumps(app.current_event.raw_event, default=str)}")  # Log the raw event data
+        request_body = app.current_event.json_body
+        email = request_body.get('email') if request_body else None
+        if not email:
+            logger.warning("Email field is missing in the request body")
+            fbpLog("fbpadmin@my-fbp.com", "AddOrUpdateFBPUser", "Email field is missing in the request body", "ERROR")
+            return {
+                'statusCode': 400,
+                'body': json.dumps({
+                    'error': 'Email address is required',
+                    'message': 'Please provide email address in the event'
+                })
+            }
+        logger.info(f"Extracted email from API Gateway event: {email}")
+        logger.info(f"Request body: {request_body}")
+        if not request_body:
+            logger.error("No JSON body found in the request")
+            fbpLog("fbpadmin@my-fbp.com", "AddOrUpdateFBPUser", "No JSON body found in the request", "ERROR")
+            return {
+                'statusCode': 400,
+                'body': json.dumps({
+                    'error': 'Invalid request body',
+                    'message': 'Request body seems to be empty or not valid JSON'
+                })
+            }
+
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+        fbpLog("fbpadmin@my-fbp.com", "AddOrUpdateFBPUser", f"Unexpected error: {e}", "ERROR")
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'error': 'Invalid request body',
+                'message': 'Request body must be valid JSON with an email field'
+            })
+        }
+
+    item = addFBPUserData(request_body)
+    
+    if item:
+        fbpLog("fbpadmin@my-fbp.com", "AddOrUpdateFBPUser", f"Updated user data for email: {email}: request_body: {json.dumps(request_body, default=str)}", "INFO")
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'email': item.get('email'),
+                'defaultAlgorithm': item.get('defaultAlgorithm'),
+                'displayName': item.get('displayName'),
+                'emailGridSheet': item.get('emailGridSheet'),
+                'emailPickSheet': item.get('emailPickSheet'),
+                'emailReminders': item.get('emailReminders'),
+                'firstName': item.get('firstName'),
+                'lastName': item.get('lastName'),
+                'isAccountLocked': item.get('isAccountLocked'),
+                'isAdmin': item.get('isAdmin'),
+                'isPaidUser': item.get('isPaidUser'),
+                })
+            }
+    else:
+        logger.info(f"User not found: {email}")
+        fbpLog("fbpadmin@my-fbp.com", "AddOrUpdateFBPUser", f"User not found: {email}", "ERROR")
+        return {
+            'statusCode': 404,
+            'body': json.dumps({
+                'error': f'User with email {email} not found',
+                'email': email,
+                })
+            } 
+
+
+
         
 
 def updateFBPUserData(request_body):
@@ -148,6 +224,37 @@ def updateFBPUserData(request_body):
         fbpLog("fbpadmin@my-fbp.com", "UpdateFBPUser", f"Unexpected error: {e}", "ERROR")
         logger.error(f"Unexpected error: {e}")
         return None
+
+
+def addFBPUserData(request_body):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(USERS_TABLE_NAME)
+    try:
+        response = table.put_item(
+            Item={
+                'email': request_body.get('email'),
+                'defaultAlgorithm': request_body.get('defaultAlgorithm'),
+                'displayName': request_body.get('displayName'),
+                'emailGridSheet': bool(request_body.get('emailGridSheet')),
+                'emailPickSheet': bool(request_body.get('emailPickSheet')),
+                'emailReminders': bool(request_body.get('emailReminders')),
+                'firstName': request_body.get('firstName'),
+                'lastName': request_body.get('lastName'),
+                'isAccountLocked': bool(request_body.get('isAccountLocked')),
+                'isAdmin': bool(request_body.get('isAdmin')),
+                'isPaidUser': bool(request_body.get('isPaidUser'))
+            }
+        )
+        return response
+    except ClientError as e:
+        logger.error(f"DynamoDB Error: {e}")
+        fbpLog("fbpadmin@my-fbp.com", "AddFBPUser", f"DynamoDB Error: {e}", "ERROR")
+        return None
+    except Exception as e:
+        fbpLog("fbpadmin@my-fbp.com", "AddFBPUser", f"Unexpected error: {e}", "ERROR")
+        logger.error(f"Unexpected error: {e}")
+        return None
+
 
 def lambda_handler(event, context):
     return app.resolve(event, context)
